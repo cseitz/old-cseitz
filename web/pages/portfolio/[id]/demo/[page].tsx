1ceeb76a6db0d68c2fb954709e0bd3426d0786ea
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-04-29 15:58:30 -0400

    working portfolio

--- web/pages/portfolio/[id]/demo/[page].tsx
+++ web/pages/portfolio/[id]/demo/[page].tsx
@@ -1,13 +1,42 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Paper, Typography } from "@mui/material";
 import { useRouter } from "next/router";
+import { useEffect, useLayoutEffect, useMemo, useRef } from "react";
 
 export default function DemoPage() {
     const router = useRouter();
-    const { query } = router;
+    const { query, asPath } = router;
     const { id } = query;
     const page = decodeURIComponent('/' + (query?.page as string || ''));
-    return <Box>
-        <Typography>Portfolio Entry '{id}'</Typography>
-        <Typography>Page: {page}</Typography>
+    const ref = useRef<HTMLIFrameElement>(null);
+    useEffect(() => {
+        if (typeof window == 'undefined') return;
+        const listener = function(evt) {
+            if ('location' in evt.data) {
+                const path: string = evt.data.location;
+                if (page != path) {
+                    router.replace(asPath, asPath.split('/demo')[0] + '/demo' + path, {
+                        shallow: true
+                    })
+                }
+            }
+        }
+        window.addEventListener('message', listener);
+        return () => window.removeEventListener('message', listener);
+    }, [asPath]);
+    const doc = (typeof window == 'undefined') ? { domain: 'localhost' } : document;
+    const iFrame = useMemo(() => {
+        if (!router.isReady) return;
+        return <iframe ref={ref} src={"http://" + id + ".portfolio." + doc.domain + page} style={{ height: '100%', width: '100%', border: 'none' }} />
+    }, [doc?.domain, router.isReady]);
+    return <Box sx={{ overflow: 'hidden' }}>
+        <Box sx={{ position: 'fixed', height: '100vh', width: '100vw', top: '0px', left: '0px' }}>
+            {iFrame}
+        </Box>
+        <Box sx={{ position: 'fixed', top: '10vh' }}>
+            <Paper>
+                <Typography>Portfolio Entry '{id}'</Typography>
+                <Typography>Page: {page}</Typography>
+            </Paper>
+        </Box>
     </Box>
 }
